cb0b4bd58ba12983758b877bdfbb909692ac2a92
Krish Jain <79595121+kri5h92@users.noreply.github.com>
2023-04-08 12:48:56 +0530

    Time: 41 ms (52.14%), Space: 20.5 MB (65.49%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -6,36 +6,16 @@ public:
         {
             return ans;
         }
-        vector<unordered_map<char,int>> vmp;
-        for(int i = 0; i < strs.size(); i++)
+        unordered_map<string,vector<string>> mp;
+        for(string s: strs)
         {
-            unordered_map<char,int> mp;
-            for(int j = 0; j < strs[i].size(); j++)
-            {
-                mp[strs[i][j]]++;
-            }
-            if(i == 0)
-            {
-                ans.push_back({strs[i]});
-                vmp.push_back(mp);
-            }
-            else 
-            {
-                bool flag = false;
-                for(int k = 0; k < vmp.size(); k++)
-                {
-                    if(mp == vmp[k])
-                    {
-                        ans[k].push_back(strs[i]);
-                        flag = true;
-                    }
-                }
-                if(!flag)
-                {
-                    vmp.push_back(mp);
-                    ans.push_back({strs[i]});
-                }
-            }
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        for(auto i : mp)
+        {
+            ans.push_back(i.second);
         }
         return ans;
     }
