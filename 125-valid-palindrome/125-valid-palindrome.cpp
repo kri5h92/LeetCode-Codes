3883b4ae591d3ce855a299f2d7057ef978080c65
Krish Jain <79595121+kri5h92@users.noreply.github.com>
2023-06-25 11:12:09 +0530

    Time: 8 ms (33.90%), Space: 7.3 MB (73.10%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string ns = "";
+        // string ns = "";
+        int x = 0;
         for(char i : s)
         {
            if(iswalnum(i))
            {
                if(i >= 65 && i <= 90)
-                   ns+=(i+32);
+                   s[x++]=(i+32);
                else 
-                   ns+=i;
+                   s[x++]=i;
            }
         }
-        int i = 0, j = ns.size() - 1;
+        cout<<s << " "<<x;
+        int i = 0, j = x-1;
         while(i < j)
         {
-            if(ns[i++] != ns[j--])
+            if(s[i++] != s[j--])
                 return false;
         }
         return true;
