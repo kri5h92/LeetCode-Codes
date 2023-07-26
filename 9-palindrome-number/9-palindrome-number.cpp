d39fb1f1fadb29716b3ee5c1b18a3c46872cff99
Krish Jain <79595121+kri5h92@users.noreply.github.com>
2023-07-26 13:05:56 +0530

    Time: 23 ms (24.75%), Space: 5.8 MB (80.50%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long t = 0, c = x;
+        while(x>0)
+        {
+            t = t*10 + x%10;
+            x/=10;
+        }
+        bool ans = (t==c) ? true : false;
+        return ans;
+        
+        
+    }
+};
