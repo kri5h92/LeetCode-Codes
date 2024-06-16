d692250d93bddfcbcd45fb019c014edbd7d66412
Krish Jain <79595121+kri5h92@users.noreply.github.com>
2024-06-16 16:33:03 +0530

    Time: 3 ms (93.43%), Space: 8.9 MB (24.87%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long t = 0, c = x;
+        string num = to_string(x);
+        int n = num.length();
+        for(int i = 0; i < n/2; i++)
+        {
+            if(num[i] != num[n-i-1])
+                return false;
+        }
+        return true;
+        // while(x>0)
+        // {
+        //     t = t*10 + x%10;
+        //     x/=10;
+        // }
+        // // bool ans = (t==c) ? true : false;
+        // return (t==c);
+        
+        
+    }
+};
